3748cd524c240df3123c6c19d738e81294d3f78a
mjeffery <michael.jeffery@gmail.com>
2014-04-26 02:45:19 -0400

    grunt build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,22 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		connect: {
+			options: {
+				hostname: 'localhost',
+				open: true
+			},
 			dev: {
 				options: {
-					hostname: 'localhost',
-					base: ['./src', './bower_components'],
+					base: ['./bower_components', './src'],
 					livereload: true,
-					open: true
 				}
-			}
+			},
+			dist: {
+				options: {
+					base: ['./dist'],
+					keepalive: true
+				}
+			} 
 		},
 		watch: {
 			livereload: {
@@ -19,8 +27,90 @@ module.exports = function(grunt) {
 					livereload: true
 				}
 			}
+		},
+		clean: {
+			build: ['.tmp/**', 'dist/**']
+		},
+		copy: {
+			build: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['index.html', 'style.css'],
+					dest: 'dist'
+				}]
+			}
+		},
+		filerev: {
+			assets: {
+				src: 'src/assets/**/*.{png,mp3,ogg,json,xml}',
+				dest: 'dist/assets'
+			}
+		},
+		filerev_replace: {
+			assets: {
+				src: '.tmp/concat/scripts.js',
+				options: {
+					assets_root: 'dist/', 
+					view_root: 'src/'
+				}	
+			}
+		},
+		useminPrepare: {
+		html: 'src/index.html',	
+			options: {
+				dest: 'dist'
+			}
+		},
+		usemin: {
+			html: 'dist/index.html',
+			options: {
+				dirs: ['dist']
+			}
 		}
 	});
 
 	grunt.registerTask('serve', ['connect:dev', 'watch']);
+
+	grunt.registerTask('dist', ['build', 'connect:dist']);
+
+	grunt.registerTask('build', [
+		'clean:build', 
+		'useminPrepare',
+		'concat',
+		'uglify',
+		'copy:build', 
+		'filerev',
+		'usemin',
+		'replacePrepare',
+		'replace'
+	]);
+
+	function escape_for_regexp( string ) {
+    	return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  	}
+
+	grunt.registerTask('replacePrepare', function() {
+		var summary = grunt.filerev.summary;
+
+		var replace = {
+			generated: {
+				src: 'dist/scripts.js',
+				overwrite: true,
+				replacements: []
+			}
+		};
+
+		for(var key in summary) {
+			var newKey = escape_for_regexp(key.substring(4).replace(/\\/g, '/'));   //remove src/
+			var newVal = '"' + summary[key].substring(5).replace(/\\/g, '/') + '"'; //remove dist/
+
+			replace.generated.replacements.push({
+				from: new RegExp('[\'"]' + newKey + '[\'"]', 'ig'),
+				to: newVal
+			});
+		}
+
+		grunt.config.set('replace', replace);
+	});
 }
